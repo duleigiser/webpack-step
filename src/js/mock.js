f13cbf4364fc821a0cf77c42adfdd6a7718fc938
dulei <564779666@qq.com>
2017-06-09 14:04:52 +0800

    添加新的电厂

--- src/js/mock.js
+++ src/js/mock.js
@@ -1,8 +1,14 @@
 var org =  [{
-name: "岱海发电",
-id:"c21834b4-1cb0-490f-a2a8-deeaf7f7e065"
+    name: "岱海发电",
+    id:"c21834b4-1cb0-490f-a2a8-deeaf7f7e065"
 }, {
-name: "宁东发电",
-id:"472212af-1977-462b-a74a-a1f36ed6562d"
+    name: "宁东发电",
+    id:"472212af-1977-462b-a74a-a1f36ed6562d"
+},{
+    name:"京桥热电",
+    id:"4961c78b-178d-423e-bec4-453fc11262cd"
+},{
+    name:"京阳热电",
+    id:"4961c78b-178d-423e-bec4-453fc11262cd "
 }]
 export default org
